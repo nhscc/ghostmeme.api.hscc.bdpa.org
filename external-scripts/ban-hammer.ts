bebd86e4497b0a39d886cd6f858843ed8028b6a8
Xunnamius (Romulus) <Xunnamius@users.noreply.github.com>
2021-07-28 17:36:42 -0700

    refactor(external-scripts): modernize ban-hammer

--- external-scripts/ban-hammer.ts
+++ external-scripts/ban-hammer.ts
@@ -1,249 +1,256 @@
+import { name as pkgName } from 'package';
 import { getEnv } from 'universe/backend/env';
-import { getDb, closeDb } from 'universe/backend/db';
 import { AppError } from 'universe/backend/error';
+import { getDb, closeDb } from 'universe/backend/db';
+import debugFactory from 'debug';
+
+const debugNamespace = `${pkgName}:prune-data`;
 
 const oneSecondInMs = 1000;
+const log = debugFactory(debugNamespace);
+const debug = debugFactory(debugNamespace);
 
-// TODO: replace AppError with IllegalExternalEnvironmentError
-export default async function main(isCLI = false) {
-  try {
-    isCLI && console.log('[ initializing ]');
+// eslint-disable-next-line no-console
+log.log = console.info.bind(console);
 
-    const {
-      BAN_HAMMER_WILL_BE_CALLED_EVERY_SECONDS: calledEverySeconds,
-      BAN_HAMMER_MAX_REQUESTS_PER_WINDOW: maxRequestsPerWindow,
-      BAN_HAMMER_RESOLUTION_WINDOW_SECONDS: resolutionWindowSeconds,
-      BAN_HAMMER_DEFAULT_BAN_TIME_MINUTES: defaultBanTimeMinutes,
-      BAN_HAMMER_RECIDIVISM_PUNISH_MULTIPLIER: punishMultiplier,
-      EXTERNAL_SCRIPTS_BE_VERBOSE: beVerbose
-    } = getEnv();
-
-    if (
-      !calledEverySeconds ||
-      !(Number(calledEverySeconds) > 0) ||
-      !maxRequestsPerWindow ||
-      !(Number(maxRequestsPerWindow) > 0) ||
-      !resolutionWindowSeconds ||
-      !(Number(resolutionWindowSeconds) > 0) ||
-      !defaultBanTimeMinutes ||
-      !(Number(defaultBanTimeMinutes) > 0) ||
-      !punishMultiplier ||
-      !(Number(punishMultiplier) > 0)
-    ) {
-      throw new AppError('illegal environment detected, check environment variables');
-    }
-
-    const calledEveryMs = oneSecondInMs * calledEverySeconds;
-    const defaultBanTimeMs = oneSecondInMs * 60 * defaultBanTimeMinutes;
-    const resolutionWindowMs = oneSecondInMs * resolutionWindowSeconds;
-
-    isCLI && console.log(`[ connecting to external database ]`);
-
-    const db = await getDb({ external: true });
-
-    isCLI && console.log(`[ running aggregate pipeline on request-log ]`);
-
-    const pipeline = [
-      {
-        $limit: 1
-      },
-      {
-        $project: { _id: 1 }
-      },
-      {
-        $project: { _id: 0 }
-      },
-      {
-        $lookup: {
-          from: 'request-log',
-          as: 'keyBased',
-          pipeline: [
-            {
-              $match: {
-                key: { $ne: null },
-                $expr: {
-                  $gte: ['$time', { $subtract: [{ $toLong: '$$NOW' }, calledEveryMs] }]
-                }
-              }
-            },
-            {
-              $group: {
-                _id: {
-                  key: '$key',
-                  interval: {
-                    $subtract: ['$time', { $mod: ['$time', resolutionWindowMs] }]
-                  }
-                },
-                count: { $sum: 1 }
-              }
-            },
-            {
-              $match: {
-                count: { $gt: maxRequestsPerWindow }
-              }
-            },
-            {
-              $project: {
-                key: '$_id.key',
-                until: { $add: [{ $toLong: '$$NOW' }, defaultBanTimeMs] }
-              }
-            },
-            {
-              $project: {
-                _id: 0,
-                count: 0
-              }
-            }
-          ]
-        }
-      },
-      {
-        $lookup: {
-          from: 'request-log',
-          as: 'ipBased',
-          pipeline: [
-            {
-              $match: {
-                $expr: {
-                  $gte: ['$time', { $subtract: [{ $toLong: '$$NOW' }, calledEveryMs] }]
-                }
-              }
-            },
-            {
-              $group: {
-                _id: {
-                  ip: '$ip',
-                  interval: {
-                    $subtract: ['$time', { $mod: ['$time', resolutionWindowMs] }]
-                  }
-                },
-                count: { $sum: 1 }
-              }
-            },
-            {
-              $match: {
-                count: { $gt: maxRequestsPerWindow }
-              }
-            },
-            {
-              $project: {
-                ip: '$_id.ip',
-                until: { $add: [{ $toLong: '$$NOW' }, defaultBanTimeMs] }
-              }
-            },
-            {
-              $project: {
-                _id: 0,
-                count: 0
-              }
-            }
-          ]
-        }
-      },
-      {
-        $lookup: {
-          from: 'limited-log-mview',
-          as: 'previous',
-          pipeline: [
-            {
-              $match: {
-                $expr: {
-                  $gte: [
-                    '$until',
-                    {
-                      $subtract: [
-                        { $toLong: '$$NOW' },
-                        defaultBanTimeMs * punishMultiplier
-                      ]
-                    }
-                  ]
-                }
-              }
-            },
-            {
-              $project: {
-                _id: 0
-              }
-            }
-          ]
-        }
-      },
-      {
-        $project: {
-          union: { $concatArrays: ['$keyBased', '$ipBased', '$previous'] }
-        }
-      },
-      {
-        $unwind: {
-          path: '$union'
-        }
-      },
-      {
-        $replaceRoot: {
-          newRoot: '$union'
-        }
-      },
-      {
-        $group: {
-          _id: {
-            ip: '$ip',
-            key: '$key'
-          },
-          count: {
-            $sum: 1
-          },
-          until: {
-            $max: '$until'
-          }
-        }
-      },
-      {
-        $set: {
-          until: {
-            $cond: {
-              if: { $ne: ['$count', 1] },
-              then: {
-                $max: [
-                  { $add: [{ $toLong: '$$NOW' }, defaultBanTimeMs * punishMultiplier] },
-                  '$until'
-                ]
-              },
-              else: '$until'
-            }
-          },
-          ip: '$_id.ip',
-          key: '$_id.key'
-        }
-      },
-      {
-        $project: {
-          count: 0,
-          _id: 0
-        }
-      },
-      {
-        $out: 'limited-log-mview'
-      }
-    ];
-
-    const cursor = db.collection('request-log').aggregate(pipeline);
-
-    isCLI && beVerbose && console.dir(pipeline, { depth: null });
-
-    await cursor.next();
-
-    isCLI && console.log('[ closing connection ]');
-
-    await cursor.close();
-    await closeDb();
-
-    isCLI && console.log('[ execution complete ]');
-  } catch (e) {
-    if (isCLI) {
-      console.error('EXCEPTION:', e);
-      process.exit(1);
-    } else throw e;
-  }
+if (!getEnv().DEBUG && getEnv().NODE_ENV != 'test') {
+  debugFactory.enable(`${debugNamespace},${debugNamespace}:*`);
+  debug.enabled = false;
 }
 
-!module.parent && main(true);
+// TODO: replace AppError with IllegalExternalEnvironmentError
+export default async function main() {
+  log('initializing');
+
+  const {
+    BAN_HAMMER_WILL_BE_CALLED_EVERY_SECONDS: calledEverySeconds,
+    BAN_HAMMER_MAX_REQUESTS_PER_WINDOW: maxRequestsPerWindow,
+    BAN_HAMMER_RESOLUTION_WINDOW_SECONDS: resolutionWindowSeconds,
+    BAN_HAMMER_DEFAULT_BAN_TIME_MINUTES: defaultBanTimeMinutes,
+    BAN_HAMMER_RECIDIVISM_PUNISH_MULTIPLIER: punishMultiplier
+  } = getEnv();
+
+  if (!calledEverySeconds || !(Number(calledEverySeconds) > 0)) {
+    throw new AppError(
+      'BAN_HAMMER_WILL_BE_CALLED_EVERY_SECONDS must be greater than zero'
+    );
+  }
+
+  if (!maxRequestsPerWindow || !(Number(maxRequestsPerWindow) > 0)) {
+    throw new AppError('BAN_HAMMER_MAX_REQUESTS_PER_WINDOW must be greater than zero');
+  }
+
+  if (!resolutionWindowSeconds || !(Number(resolutionWindowSeconds) > 0)) {
+    throw new AppError('BAN_HAMMER_RESOLUTION_WINDOW_SECONDS must be greater than zero');
+  }
+
+  if (!defaultBanTimeMinutes || !(Number(defaultBanTimeMinutes) > 0)) {
+    throw new AppError('BAN_HAMMER_DEFAULT_BAN_TIME_MINUTES must be greater than zero');
+  }
+
+  if (!punishMultiplier || !(Number(punishMultiplier) > 0)) {
+    throw new AppError(
+      'BAN_HAMMER_RECIDIVISM_PUNISH_MULTIPLIER must be greater than zero'
+    );
+  }
+
+  const calledEveryMs = oneSecondInMs * calledEverySeconds;
+  const defaultBanTimeMs = oneSecondInMs * 60 * defaultBanTimeMinutes;
+  const resolutionWindowMs = oneSecondInMs * resolutionWindowSeconds;
+
+  log('connecting to external database');
+
+  const db = await getDb({ external: true });
+
+  const pipeline = [
+    {
+      $limit: 1
+    },
+    {
+      $project: { _id: 1 }
+    },
+    {
+      $project: { _id: 0 }
+    },
+    {
+      $lookup: {
+        from: 'request-log',
+        as: 'keyBased',
+        pipeline: [
+          {
+            $match: {
+              key: { $ne: null },
+              $expr: {
+                $gte: ['$time', { $subtract: [{ $toLong: '$$NOW' }, calledEveryMs] }]
+              }
+            }
+          },
+          {
+            $group: {
+              _id: {
+                key: '$key',
+                interval: {
+                  $subtract: ['$time', { $mod: ['$time', resolutionWindowMs] }]
+                }
+              },
+              count: { $sum: 1 }
+            }
+          },
+          {
+            $match: {
+              count: { $gt: maxRequestsPerWindow }
+            }
+          },
+          {
+            $project: {
+              key: '$_id.key',
+              until: { $add: [{ $toLong: '$$NOW' }, defaultBanTimeMs] }
+            }
+          },
+          {
+            $project: {
+              _id: 0,
+              count: 0
+            }
+          }
+        ]
+      }
+    },
+    {
+      $lookup: {
+        from: 'request-log',
+        as: 'ipBased',
+        pipeline: [
+          {
+            $match: {
+              $expr: {
+                $gte: ['$time', { $subtract: [{ $toLong: '$$NOW' }, calledEveryMs] }]
+              }
+            }
+          },
+          {
+            $group: {
+              _id: {
+                ip: '$ip',
+                interval: {
+                  $subtract: ['$time', { $mod: ['$time', resolutionWindowMs] }]
+                }
+              },
+              count: { $sum: 1 }
+            }
+          },
+          {
+            $match: {
+              count: { $gt: maxRequestsPerWindow }
+            }
+          },
+          {
+            $project: {
+              ip: '$_id.ip',
+              until: { $add: [{ $toLong: '$$NOW' }, defaultBanTimeMs] }
+            }
+          },
+          {
+            $project: {
+              _id: 0,
+              count: 0
+            }
+          }
+        ]
+      }
+    },
+    {
+      $lookup: {
+        from: 'limited-log-mview',
+        as: 'previous',
+        pipeline: [
+          {
+            $match: {
+              $expr: {
+                $gte: [
+                  '$until',
+                  {
+                    $subtract: [{ $toLong: '$$NOW' }, defaultBanTimeMs * punishMultiplier]
+                  }
+                ]
+              }
+            }
+          },
+          {
+            $project: {
+              _id: 0
+            }
+          }
+        ]
+      }
+    },
+    {
+      $project: {
+        union: { $concatArrays: ['$keyBased', '$ipBased', '$previous'] }
+      }
+    },
+    {
+      $unwind: {
+        path: '$union'
+      }
+    },
+    {
+      $replaceRoot: {
+        newRoot: '$union'
+      }
+    },
+    {
+      $group: {
+        _id: {
+          ip: '$ip',
+          key: '$key'
+        },
+        count: {
+          $sum: 1
+        },
+        until: {
+          $max: '$until'
+        }
+      }
+    },
+    {
+      $set: {
+        until: {
+          $cond: {
+            if: { $ne: ['$count', 1] },
+            then: {
+              $max: [
+                { $add: [{ $toLong: '$$NOW' }, defaultBanTimeMs * punishMultiplier] },
+                '$until'
+              ]
+            },
+            else: '$until'
+          }
+        },
+        ip: '$_id.ip',
+        key: '$_id.key'
+      }
+    },
+    {
+      $project: {
+        count: 0,
+        _id: 0
+      }
+    },
+    {
+      $out: 'limited-log-mview'
+    }
+  ];
+
+  debug('aggregation pipeline: %O', pipeline);
+
+  const cursor = db.collection('request-log').aggregate(pipeline);
+  await cursor.next();
+  cursor.close();
+
+  debug('closing connection');
+  await closeDb();
+  log('execution complete');
+}
+
+!module.parent && main().catch((e) => log.extend('exception')(e.message || e.toString()));
