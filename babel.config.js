50134ded9e872698bc2c4dcb164bb7c88a35d854
Xunnamius (Romulus) <Xunnamius@users.noreply.github.com>
2021-08-27 18:55:32 -0700

    chore(package): update dependencies

--- babel.config.js
+++ babel.config.js
@@ -45,14 +45,14 @@ module.exports = {
   plugins: [
     '@babel/plugin-proposal-export-default-from',
     '@babel/plugin-proposal-function-bind',
-    '@babel/plugin-transform-typescript',
+    '@babel/plugin-transform-typescript'
     // ? Interoperable named CJS imports for free
-    [
-      'transform-default-named-imports',
-      {
-        exclude: [/^next([/?#].+)?/, /^mongodb([/?#].+)?/]
-      }
-    ]
+    // [
+    //   'transform-default-named-imports',
+    //   {
+    //     exclude: [/^next([/?#].+)?/, /^mongodb([/?#].+)?/]
+    //   }
+    // ]
   ],
   // ? Sub-keys under the "env" config key will augment the above
   // ? configuration depending on the value of NODE_ENV and friends. Default
