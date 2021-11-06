66449cb876dd28a0e4bfb711d335004d5152a6ff
Xunnamius (Romulus) <Xunnamius@users.noreply.github.com>
2021-11-05 17:28:23 -0700

    chore(package): update dependencies

---
+++ .ncurc.js
@@ -0,0 +1,8 @@
+// * https://www.npmjs.com/package/npm-check-updates#configuration-files
+
+module.exports = {
+  reject: [
+    // ? Pin eslint to ^7 until eslint-config-next updates its peer deps
+    'eslint'
+  ]
+};
